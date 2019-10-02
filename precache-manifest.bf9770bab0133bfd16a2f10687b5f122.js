9956c7339c26afa3c1a00e6dcedbf7ff95841a10
Alexander Sulema <assulema@gmail.com>
2019-10-02 21:23:36 +0300

    Updates

---
+++ precache-manifest.bf9770bab0133bfd16a2f10687b5f122.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bcb9d898c48de0c40b009becbfa4e9",
+    "url": "/querySolTest/index.html"
+  },
+  {
+    "revision": "f150a15b017b9cd2dfd7",
+    "url": "/querySolTest/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "6948488dfb15cab1a0b5",
+    "url": "/querySolTest/static/css/main.e1a5ef03.chunk.css"
+  },
+  {
+    "revision": "f150a15b017b9cd2dfd7",
+    "url": "/querySolTest/static/js/2.a9b25044.chunk.js"
+  },
+  {
+    "revision": "6948488dfb15cab1a0b5",
+    "url": "/querySolTest/static/js/main.f58ec23f.chunk.js"
+  },
+  {
+    "revision": "4772d6220809ba755a52",
+    "url": "/querySolTest/static/js/runtime-main.a7fc2314.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/querySolTest/static/media/LatoRegular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "44a95d7133c550f04675cc7ebddd47dd",
+    "url": "/querySolTest/static/media/almoni-dl-aaa-bold.44a95d71.otf"
+  },
+  {
+    "revision": "eee00615d2cf1d32e33d82db6ec5a80f",
+    "url": "/querySolTest/static/media/almoni-dl-aaa-regular.eee00615.otf"
+  },
+  {
+    "revision": "d3323cf23f35d63698ec6d8af7010b29",
+    "url": "/querySolTest/static/media/auditorium.d3323cf2.png"
+  },
+  {
+    "revision": "05936ee0cdb0b9d68eaf531f0a824ed4",
+    "url": "/querySolTest/static/media/choosen.05936ee0.png"
+  },
+  {
+    "revision": "2819d401de7e66717db5b238a848b277",
+    "url": "/querySolTest/static/media/electricVibe.2819d401.ttf"
+  },
+  {
+    "revision": "097d75fc72e3e713a0028c5cd2a6b6b1",
+    "url": "/querySolTest/static/media/figureBG.097d75fc.png"
+  },
+  {
+    "revision": "66fa5c52d53d9511b7a3cfb2d3533e6b",
+    "url": "/querySolTest/static/media/headerMainPic1.66fa5c52.png"
+  },
+  {
+    "revision": "c70b73e9726018201e651a267e88665a",
+    "url": "/querySolTest/static/media/headerMainPic2.c70b73e9.png"
+  },
+  {
+    "revision": "a86564a2f5c1c80d088e7bdc42832405",
+    "url": "/querySolTest/static/media/headerMainPic3.a86564a2.png"
+  },
+  {
+    "revision": "a22b1ddfe37673f4b0cb2ce4ed661f24",
+    "url": "/querySolTest/static/media/mic.a22b1ddf.png"
+  },
+  {
+    "revision": "c718039acfe9ce7d7328b09759982a81",
+    "url": "/querySolTest/static/media/onlineMic.c718039a.png"
+  }
+]);
